73e8d64a0d19edec1451d59af7d6813c1c3c4aaf
muhammad-ilham21 <ilham2003921@gmail.com>
2025-11-27 09:52:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const decreaseBtn = document.getElementById("decreaseBtn");
 const resetBtn = document.getElementById("resetBtn");
 const increasBtn = document.getElementById("increasBtn");
 const Countlable = document.getElementById("lb1");
-let count = 100;
+let count = 0;
 
 increasBtn.onclick = function(){
     count++;
@@ -13,13 +13,9 @@ decreaseBtn.onclick = function(){
     Countlable.textContent = count;
 }
 resetBtn.onclick = function(){
-    count = 100;
+    count = 0;
     Countlable.textContent = count;
 }
-#lb2{
-    font-size: 2em;
-    font-family:Arial, Helvetica, sans-serif;
-    font-style: italic;
-    color: rgb(237, 147, 51);
-}
+
+
 
